868b1eeebb8c692e3c33efad2b1f59912b5f8e4b
Artem <132654925+piqee@users.noreply.github.com>
2025-07-22 11:54:22 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux btop fastfetch 
 
+dnf5 install -y river waybar
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
