9ddafad8a72a4999d688122b97342744005e75d6
Artem <132654925+piqee@users.noreply.github.com>
2025-07-22 13:49:06 +0300

    Update sddm.sh

--- build_files/sddm.sh
+++ build_files/sddm.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -ouex pipefail
 
-echo "Настройка файла конфигурации SDDM (/etc/sddm.conf)..."
+ echo "Настройка файла конфигурации SDDM (/etc/sddm.conf)..."
 
 SDDM_CONF="/etc/sddm.conf"
 
+# Создаем файл конфигурации, если он не существует
 if [ ! -f "$SDDM_CONF" ]; then
-  echo "[Theme]" | tee "$SDDM_CONF" > /dev/null
-  echo "Current=breeze" | tee -a "$SDDM_CONF" > /dev/null
-  echo "" | tee -a "$SDDM_CONF" > /dev/null
-  echo "[Autologin]" | tee -a "$SDDM_CONF" > /dev/null
-  echo "[Wayland]" | tee -a "$SDDM_CONF" > /dev/null
-  echo "[X11]" | tee -a "$SDDM_CONF" > /dev/null
-  echo "[General]" | tee -a "$SDDM_CONF" > /dev/null
-  echo "Создан базовый файл $SDDM_CONF."
+    echo "[Theme]" | sudo tee "$SDDM_CONF" > /dev/null
+    echo "Current=breeze" | sudo tee -a "$SDDM_CONF" > /dev/null
+    echo "" | sudo tee -a "$SDDM_CONF" > /dev/null  
+    echo "[Autologin]" | sudo tee -a "$SDDM_CONF" > /dev/null
+    echo "[Wayland]" | sudo tee -a "$SDDM_CONF" > /dev/null
+    echo "[X11]" | sudo tee -a "$SDDM_CONF" > /dev/null
+    echo "[General]" | sudo tee -a "$SDDM_CONF" > /dev/null
+    echo "Создан базовый файл $SDDM_CONF."
 fi
