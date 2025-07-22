acd0827204f07b1472f7490e019915a85f706032
Artem <132654925+piqee@users.noreply.github.com>
2025-07-22 18:33:42 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux btop fastfetch foot grim thunar \
+dnf5 install -y tmux btop fastfetch fuzzel foot grim thunar \
                 wlr-randr adw-gtk3-theme qt6ct #themes/libs
 
 dnf5 install -y river waybar sddm
