0529b71a5e94a16640e7c7752492271ef70265d2
Artem <132654925+piqee@users.noreply.github.com>
2025-07-22 09:30:25 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 install -y tmux btop fastfetch 
 
 # Use a COPR Example:
 #
 dnf5 -y copr enable ublue-os/staging
 dnf5 -y install package
+
+dnf5 -y copr enable atim/starship
+dnf5 -y install starship
+
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable atim/starship
 
 #### Example for enabling a System Unit File
 
