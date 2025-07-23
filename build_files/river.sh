ab7ebf30d90eeea872a07a265e3466de7e0c3156
Artem <132654925+piqee@users.noreply.github.com>
2025-07-23 23:28:55 +0300

    Update river.sh

--- build_files/river.sh
+++ build_files/river.sh
@@ -6,19 +6,20 @@ echo "=== Configuring RiverWM ==="
 # Создание директории для сессии
 mkdir -p /usr/share/wayland-sessions/
 
-# Создание .desktop файла для RiverWM
-# cat > /usr/share/wayland-sessions/river.desktop << 'EOF'
-# [Desktop Entry]
-# Name=River
-# Comment=Dynamic tiling Wayland compositor
-# Exec=river
-# Type=Application
-# DesktopNames=River
-# EOF
+Создание .desktop файла для RiverWM
+cat > /usr/share/wayland-sessions/river.desktop << 'EOF'
+[Desktop Entry]
+Name=River
+Comment=Dynamic tiling Wayland compositor
+Exec=river
+Type=Application
+DesktopNames=River
+EOF
 
 # Создание базовой конфигурации для пользователя
 
 mkdir -p /etc/skel/.config/river/
+touch /etc/skel/.config/river/init
 
 cat > /etc/skel/.config/river/init << 'EOF'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       init                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 #!/bin/sh
