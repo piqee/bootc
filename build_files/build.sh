581c0e1e8e8953e62d690d6e161b92143a222137
Artem <132654925+piqee@users.noreply.github.com>
2025-07-24 00:21:44 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,11 +2,28 @@
 
 set -ouex pipefail
 
-dnf5 install -y tmux btop fastfetch fuzzel foot grim thunar gnome-software lxappearance \
-                swaybg \
-                wlr-randr adw-gtk3-theme qt6ct #themes/libs
-
-dnf5 install -y river waybar sddm
+dnf5 install -y tmux btop fastfetch fuzzel foot thunar gnome-software lxappearance swaybg 
+dnf5 install -y \
+    river \
+    waybar \
+    wlroots \
+    xdg-desktop-portal-wlr \
+    sddm \
+    sddm-theme-maldives \
+    foot \
+    brightnessctl \
+    swayidle \
+    swaylock \
+    grim \
+    slurp \
+    wl-clipboard \
+    adw-gtk3-theme \
+    gnome-themes-extra \
+    adwaita-qt5 \
+    adwaita-qt6 \
+    qt5-qtconfiguration \
+    qt5ct \
+    qt6ct
 
 dnf5 -y copr enable atim/starship
 dnf5 -y install starship
