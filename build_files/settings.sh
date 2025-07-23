71d1ccc673f43af4682246e03252bfe94f6c7a49
Artem <132654925+piqee@users.noreply.github.com>
2025-07-24 00:23:56 +0300

    Update settings.sh

--- build_files/settings.sh
+++ build_files/settings.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #Dark theme https://github.com/lassekongo83/adw-gtk3
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
